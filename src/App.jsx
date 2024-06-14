fafd5157c3140394e22bc8c11da2e25b4d33be57
ValeriyaHr <152651276+ValeriyaHr@users.noreply.github.com>
2024-06-14 19:47:19 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Profile from "../Profile/Profile";
-import userData from "../../userData.json";
-import FriendList from "../FriendList/FriendList";
-import friends from "../../friends.json";
-import TransactionHistory from "../TransactionHistory/TransactionHistory";
-import transactions from "../../transactions.json";
+import Profile from "./components/Profile/Profile";
+import userData from "./userData.json";
+import FriendList from "./components/FriendList/FriendList";
+import friends from "./friends.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import transactions from "./transactions.json";
 export default function App() {
   return (
     <div className="container">
